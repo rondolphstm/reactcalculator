15b307abfc9ee4cd3ffb39a7472a73c2ed834096
rondolphstm <rondolphtst.m@gmail.com>
2022-12-29 12:10:55 -0500

    creating and styling buttons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     test
+      <div className="calc-wrapper">
+        test
+      </div>
     </div>
   );
 }
---
+++ src/components/button.jsx
@@ -0,0 +1,10 @@
+import react from "react"
+import "./button.css"
+
+const button = props => (
+    <div>
+        {props.childeren}
+    </div>
+)
+
+export default button
